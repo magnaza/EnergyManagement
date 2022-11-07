6dce1b79edd5a2d14a490ff2746fb01fd2b27fd2
magnaza <magnaza7@gmail.com>
2022-11-07 15:29:17 +0100

    modify matlab script for new reports

--- dpm-simulator/test1.m
+++ dpm-simulator/test1.m
@@ -1,16 +1,16 @@
-load reports\output1_idle.txt
-[n] = size(output1_idle)
-[n, p] = size(output1_idle)
+load reports\norm_output1_idle.txt
+[n] = size(norm_output1_idle)
+[n, p] = size(norm_output1_idle)
 t = 1:n
-table1=array2table(output1_idle)
+table1=array2table(norm_output1_idle)
 figure
 hold on
-plot(t, table1.output1_idle2)
+plot(t, table1.norm_output1_idle2)
 
-load reports\output1_sleep.txt
-[n] = size(output1_sleep)
-[n, p] = size(output1_sleep)
+load reports\norm_output1_sleep.txt
+[n] = size(norm_output1_sleep)
+[n, p] = size(norm_output1_sleep)
 t = 1:n
-table2=array2table(output1_sleep)
-plot(t, table2.output1_sleep2)
+table2=array2table(norm_output1_sleep)
+plot(t, table2.norm_output1_sleep2)
 legend('idle', 'sleep')
