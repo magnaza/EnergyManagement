144c88e1f3cee319d3c6e6d5312656ff198b5442
DAVIDE BOLOGNESI S295938 <s295938@studenti.polito.it>
2022-11-07 19:11:58 +0100

    added step

--- dpm-simulator/script_final.sh
+++ dpm-simulator/script_final.sh
@@ -1,5 +1,7 @@
 N=10
+step=0.2
 scale=8
+
 >reports/output1_idle.txt
 >reports/output1_sleep.txt
 >reports/output1_sleepidle.txt
@@ -7,44 +9,45 @@ scale=8
 >reports/norm_output1_idle.txt
 >reports/norm_output1_sleep.txt
 >reports/norm_output1_sleepidle.txt
+
 energy_nodpm=$(./dpm_simulator -psm example/psm.txt -wl workloads/workload_1.txt -ts 10 | grep -o -P '\d+\.\d*' | sed -n 22p)
 
-for (( c=0; c<=$N; c++))
+for (( c=0; c<=$(bc<<<"$N/$step"); c++))
 do
-    outcommand=$(./dpm_simulator -psm example/psm.txt -wl workloads/workload_1.txt -t $c)
-    that="$c          "
+    x=$(echo $(bc<<<"$step * $c"))
+    outcommand=$(./dpm_simulator -psm example/psm.txt -wl workloads/workload_1.txt -t $x)
     this=$(echo "$outcommand" | grep -o -P '\d+\.\d*' | sed '$!d')
-    those="$that $this"
+    those="$x $this"
     echo $those >> reports/output1_idle.txt
 
-    echo -n "$c " >> reports/norm_output1_idle.txt
+    echo -n "$x " >> reports/norm_output1_idle.txt
     echo "scale=$scale; (1 - $this/$energy_nodpm) * 100" | bc >> reports/norm_output1_idle.txt
 done
 
-for (( c=0; c<=$N; c++))
+for (( c=0; c<=$(bc<<<"$N/$step"); c++))
 do
-    outcommand=$(./dpm_simulator -psm example/psm.txt -wl workloads/workload_1.txt -ts $c)
-    that="$c          "
+    x=$(echo $(bc<<<"$step * $c"))
+    outcommand=$(./dpm_simulator -psm example/psm.txt -wl workloads/workload_1.txt -ts $x)
     this=$(echo "$outcommand" | grep -o -P '\d+\.\d*' | sed '$!d')
-    those="$that $this"
+    those="$x $this"
     echo $those >> reports/output1_sleep.txt
 
-    echo -n "$c " >> reports/norm_output1_sleep.txt
+    echo -n "$x " >> reports/norm_output1_sleep.txt
     echo "scale=$scale; (1 - $this/$energy_nodpm) * 100" | bc >> reports/norm_output1_sleep.txt
 done
 
 fixed_idle=50
 
-for (( c=0; c<=$N; c++))
+for (( c=0; c<=$(bc<<<"$N/$step"); c++))
 do
-    i=$(($c + $fixed_idle))
+    x=$(echo $(bc<<<"$step * $c"))
+    i=$(($x + $fixed_idle))
     outcommand=$(./dpm_simulator -psm example/psm.txt -wl workloads/workload_1.txt -ts $i -t $fixed_idle)
-    that="$c          "
     this=$(echo "$outcommand" | grep -o -P '\d+\.\d*' | sed '$!d')
-    those="$that $this"
+    those="$i $this"
     echo $those >> reports/output1_sleepidle.txt
 
-    echo -n "$c " >> reports/norm_output1_sleepidle.txt
+    echo -n "$x " >> reports/norm_output1_sleepidle.txt
     echo "scale=$scale; (1 - $this/$energy_nodpm) * 100" | bc >> reports/norm_output1_sleepidle.txt
 done
 
