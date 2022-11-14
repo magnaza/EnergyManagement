993b73ad6f4de713c6e105523a4c8bdd9656f871
DAVIDE BOLOGNESI S295938 <s295938@studenti.polito.it>
2022-11-14 21:51:18 +0100

    fixed a bit script

--- dpm-simulator/script_final.sh
+++ dpm-simulator/script_final.sh
@@ -1,9 +1,15 @@
-N=10
-step=0.2
-scale=8
 
+#---------------------------------------inputs------------------------------------------------------------#
+
+N=20
+step=0.1
+scale=12
+
+fixed_idle=50
 workload=workloads/workload_1.txt
 psm=example/psm.txt
+
+#---------------------------------------------------------------------------------------------------------#
 
 >reports/output1_idle.txt
 >reports/output1_sleep.txt
@@ -17,44 +23,40 @@ energy_nodpm=$(./dpm_simulator -psm $psm -wl $workload -ts 10 | grep -o -P '\d+\
 
 for (( c=0; c<=$(bc<<<"$N/$step"); c++))
 do
+    
     x=$(echo $(bc<<<"$step * $c"))
-    outcommand=$(./dpm_simulator -psm $psm -wl $workload -t $x)
-    this=$(echo "$outcommand" | grep -o -P '\d+\.\d*' | sed '$!d')
-    those="$x $this"
-    echo $those >> reports/output1_idle.txt
+
+#---------------------------------------idle--------------------------------------------------------------#
+
+    outcommand_idle=$(./dpm_simulator -psm $psm -wl $workload -t $x)
+    filtered_idle=$(echo "$outcommand_idle" | grep -o -P '\d+\.\d*' | sed '$!d')
+    text_idle="$x $filtered_idle"
+    echo $text_idle >> reports/output1_idle.txt
 
     echo -n "$x " >> reports/norm_output1_idle.txt
-    echo "scale=$scale; (1 - $this/$energy_nodpm) * 100" | bc >> reports/norm_output1_idle.txt
-done
+    echo "scale=$scale; (1 - $filtered_idle/$energy_nodpm) * 100" | bc >> reports/norm_output1_idle.txt
 
-for (( c=0; c<=$(bc<<<"$N/$step"); c++))
-do
-    x=$(echo $(bc<<<"$step * $c"))
-    outcommand=$(./dpm_simulator -psm $psm -wl $workload -ts $x)
-    this=$(echo "$outcommand" | grep -o -P '\d+\.\d*' | sed '$!d')
-    those="$x $this"
-    echo $those >> reports/output1_sleep.txt
+#----------------------------------------sleep------------------------------------------------------------#
+
+    outcommand_sleep=$(./dpm_simulator -psm $psm -wl $workload -ts $x)
+    filtered_sleep=$(echo "$outcommand_sleep" | grep -o -P '\d+\.\d*' | sed '$!d')
+    text_sleep="$x $filtered_sleep"
+    echo $text_sleep >> reports/output1_sleep.txt
 
     echo -n "$x " >> reports/norm_output1_sleep.txt
-    echo "scale=$scale; (1 - $this/$energy_nodpm) * 100" | bc >> reports/norm_output1_sleep.txt
-done
+    echo "scale=$scale; (1 - $filtered_sleep/$energy_nodpm) * 100" | bc >> reports/norm_output1_sleep.txt
 
-fixed_idle=50
+#----------------------------------------sleep&idle-------------------------------------------------------#
 
-for (( c=0; c<=$(bc<<<"$N/$step"); c++))
-do
-    x=$(echo $(bc<<<"$step * $c"))
     i=$(($x + $fixed_idle))
-    outcommand=$(./dpm_simulator -psm $psm -wl $workload -ts $i -t $fixed_idle)
-    this=$(echo "$outcommand" | grep -o -P '\d+\.\d*' | sed '$!d')
-    those="$i $this"
-    echo $those >> reports/output1_sleepidle.txt
+    outcommand_sleepidle=$(./dpm_simulator -psm $psm -wl $workload -ts $i -t $fixed_idle)
+    filtered_sleepidle=$(echo "$outcommand_sleepidle" | grep -o -P '\d+\.\d*' | sed '$!d')
+    text_sleepidle="$i $filtered_sleepidle"
+    echo $text_sleepidle >> reports/output1_sleepidle.txt
 
     echo -n "$x " >> reports/norm_output1_sleepidle.txt
-    echo "scale=$scale; (1 - $this/$energy_nodpm) * 100" | bc >> reports/norm_output1_sleepidle.txt
+    echo "scale=$scale; (1 - $filtered_sleepidle/$energy_nodpm) * 100" | bc >> reports/norm_output1_sleepidle.txt
+
 done
-
-
-
 
 #matlab.exe -r test1
