8a3c1a2841f27bc91b1f78b93882166bb2e9dc1f
magnaza <magnaza7@gmail.com>
2022-11-15 14:24:44 +0100

    modified matlab script

--- dpm-simulator/test1.m
+++ dpm-simulator/test1.m
@@ -1,16 +1,16 @@
 load reports\norm_output1_idle.txt
-[n] = size(norm_output1_idle)
-[n, p] = size(norm_output1_idle)
-t = 1:n
+%[n] = size(norm_output1_idle)
+%[n, p] = size(norm_output1_idle)
+%t = 1:n
 table1=array2table(norm_output1_idle)
 figure
 hold on
-plot(t, table1.norm_output1_idle2)
-
+plot(table1.norm_output1_idle1, table1.norm_output1_idle2)
+%legend('idle')
 load reports\norm_output1_sleep.txt
-[n] = size(norm_output1_sleep)
-[n, p] = size(norm_output1_sleep)
-t = 1:n
+%[n] = size(norm_output1_sleep)
+%[n, p] = size(norm_output1_sleep)
+%t = 1:n
 table2=array2table(norm_output1_sleep)
-plot(t, table2.norm_output1_sleep2)
+plot(table2.norm_output1_sleep1, table2.norm_output1_sleep2)
 legend('idle', 'sleep')
